6fb2e596ad61c289cc893686ac76713d0a9c0984
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-01-13 12:06:34 +1100

    inconsequential change to test new git-antenna

--- draft_sport/security/session.py
+++ draft_sport/security/session.py
@@ -7,5 +7,8 @@ from draft_sport.data.decodable import Decodable
 
 
 class Session(Decodable):
-    """An authenticated session with the Draft Sport API"""
+    """
+    An authenticated session with the Draft Sport API, storing credentials
+    for use by API requests.
+    """
     pass
