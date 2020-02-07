e4f34e75ff2bba8c4ed6d3cdd4a7d310f664f4f7
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-07 11:27:44 +1100

    add `Profile.position_name` property

--- draft_sport/fantasy/player/profile.py
+++ draft_sport/fantasy/player/profile.py
@@ -15,12 +15,14 @@ class Profile(Decodable):
         self,
         first_name: str,
         last_name: str,
+        position_name: str,
         public_id: str,
         team_name: str
     ) -> None:
 
         self._first_name = first_name
         self._last_name = last_name
+        self._position_name = position_name
         self._public_id = public_id
         self._team_name = team_name
 
@@ -29,6 +31,7 @@ class Profile(Decodable):
     first_name = Immutable(lambda s: s._first_name)
     last_name = Immutable(lambda s: s._last_name)
     public_id = Immutable(lambda s: s._public_id)
+    position_name = Immutable(lambda s: s._position_name)
     team_name = Immutable(lambda s: s._team_name)
 
     full_name = Immutable(lambda s: s._first_name + ' ' + s._last_name)
@@ -38,6 +41,7 @@ class Profile(Decodable):
         return cls(
             first_name=data['first_name'],
             last_name=data['last_name'],
+            position_name=data['position_name'],
             public_id=data['public_id'],
             team_name=data['team_name']
         )
