d1ac435b94350393b4aeb7823b509cd6deabb729
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-09 15:12:00 +1100

    add "points per minute played" to `Points`

--- draft_sport/fantasy/scores/player/points.py
+++ draft_sport/fantasy/scores/player/points.py
@@ -17,12 +17,14 @@ class Points(Decodable):
         average_points: int,
         total_points: int,
         points_last_round: int,
+        points_per_minute_played: int,
         rounds: List[Round]
     ) -> None:
 
         self._average_points = average_points
         self._total_points = total_points
         self._points_last_round = points_last_round
+        self._points_per_minute_played = points_per_minute_played
         self._rounds = rounds
 
         return
@@ -30,11 +32,13 @@ class Points(Decodable):
     average_points = Immutable(lambda s: s._average_points)
     total_points = Immutable(lambda s: s._total_points)
     points_last_round = Immutable(lambda s: s._points_last_round)
+    points_per_minute_played = Immutable(lambda s: s._points_per_minute_played)
     rounds = Immutable(lambda s: s._rounds)
 
     @classmethod
     def decode(cls: Type[T], data: Any) -> T:
         return cls(
+            points_per_minute_played=data['points_per_minute_played'],
             average_points=data['average_points'],
             total_points=data['total_points'],
             points_last_round=data['points_last_round'],
