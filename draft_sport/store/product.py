01d7b3299cced47cdc93227bae2500479b2ca95f
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-03-09 15:44:04 +1100

    add `Product.features` property

--- draft_sport/store/product.py
+++ draft_sport/store/product.py
@@ -3,8 +3,8 @@ Draft Sport Python Library
 Product Module
 author: hugh@blinkybeach.com
 """
-from nozomi import Decodable
-from typing import TypeVar, Type, Any
+from nozomi import Decodable, Immutable
+from typing import TypeVar, Type, Any, List
 
 T = TypeVar('T', bound='Product')
 
@@ -14,17 +14,22 @@ class Product(Decodable):
     def __init__(
         self,
         public_id: str,
-        name: str
+        name: str,
+        features: List[str]
     ) -> None:
 
         self._public_id = public_id
         self._name = name
+        self._features = features
 
         return
+
+    features = Immutable(lambda s: s._features)
 
     @classmethod
     def decode(cls: Type[T], data: Any) -> T:
         return cls(
             public_id=data['public_id'],
-            name=data['name']
+            name=data['name'],
+            features=data['features']
         )
